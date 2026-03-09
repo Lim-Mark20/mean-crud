ff8da3ae6a8ccbe8c46d7607ffcf8739d4275709
mark lim <mblim1220@gmail.com>
2026-03-09 16:12:29 +0800

    Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,100 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('mean-crud');
+export class AppComponent implements OnInit {
+  title = 'booksapp';
+  readonly APIUrl="http://localhost:5038/api/books/";
+
+  bookForm!: FormGroup;
+  editingBookId: string | null = null;
+  books: any = [];
+
+  constructor(private http: HttpClient, private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.createForm();
+    this.refreshBooks();
+  }
+
+  createForm() {
+    this.bookForm = this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      price: ['', [Validators.required]],
+      author: ['', Validators.required],
+      year: ['', [Validators.required]]
+    });
+  }
+
+  refreshBooks() {
+    this.http.get(this.APIUrl + 'GetBooks').subscribe(data => {
+      this.books = data;
+    });
+  }
+
+  submitBook() {
+    if (this.bookForm.invalid) {
+      this.bookForm.markAllAsTouched();
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("title", this.bookForm.value.title);
+    formData.append("description", this.bookForm.value.description);
+    formData.append("price", this.bookForm.value.price.toString());
+    formData.append("author", this.bookForm.value.author);
+    formData.append("year", this.bookForm.value.year.toString());
+
+    if (this.editingBookId) {
+      // Edit mode
+      formData.append("id", this.editingBookId);
+      this.http.put(this.APIUrl + 'UpdateBook?id=' + this.editingBookId, formData).subscribe(data => {
+        alert(data);
+        this.resetForm();
+        this.refreshBooks();
+      });
+    } else {
+      // Add mode
+      this.http.post(this.APIUrl + 'AddBook', formData).subscribe(data => {
+        alert(data);
+        this.resetForm();
+        this.refreshBooks();
+      });
+    }
+  }
+
+  editBook(book: any) {
+    this.editingBookId = book.id;
+    this.bookForm.patchValue({
+      title: book.title,
+      description: book.desc,
+      price: book.price,
+      author: book.author || '',
+      year: book.year || ''
+    });
+  }
+
+  resetForm() {
+    this.editingBookId = null;
+    this.bookForm.reset();
+  }
+
+  deleteBook(id: any) {
+    if (confirm("Are you sure you want to delete this book?")) {
+      this.http.delete(this.APIUrl + 'DeleteBook?id=' + id).subscribe(data => {
+        alert(data);
+        this.refreshBooks();
+      });
+    }
+  }
 }
